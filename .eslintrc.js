ee25816721ad0f91c5ce9777b8dd20bfc8bf3b36
Evan Brodie <brodie.evan@gmail.com>
2020-09-18 11:29:51 -0400

    Sets up typescript and eslint

---
+++ .eslintrc.js
@@ -0,0 +1,42 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier/@typescript-eslint',
+    'plugin:prettier/recommended',
+    // TODO: Add gatsby-eslint-plugin with custom options for typescript
+  ],
+  settings: {
+    react: {
+      version: 'detect'
+    }
+  },
+  env: {
+    browser: true,
+    node: true,
+    es6: true
+  },
+  plugins: ['@typescript-eslint', 'react'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 2018, // Allows for the parsing of modern ECMAScript features
+    sourceType: 'module' // Allows for the use of imports
+  },
+  rules: {
+    'react/prop-types': 'off', // TODO: Off for now, but let's bring it back later
+    '@typescript-eslint/explicit-function-return-type': 'off'
+  },
+  overrides: [
+    // Override some TypeScript rules just for .js files
+    {
+      files: ['*.js'],
+      rules: {
+        '@typescript-eslint/no-var-requires': 'off' //
+      }
+    }
+  ]
+};
