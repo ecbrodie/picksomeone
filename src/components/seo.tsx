f7866ce8041591809523dc51e4d749515373b1ae
Evan Brodie <brodie.evan@gmail.com>
2020-09-18 13:26:01 -0400

    Converts existing source code to typescript
    Also removes unnecessary scaffolding pages

--- src/components/seo.tsx
+++ src/components/seo.tsx
@@ -6,11 +6,17 @@
  */
 
 import React from "react"
-import PropTypes from "prop-types"
 import { Helmet } from "react-helmet"
 import { useStaticQuery, graphql } from "gatsby"
 
-function SEO({ description, lang, meta, title }) {
+interface Props {
+  description?: string
+  lang?: string,
+  meta?: any[], // TODO: Use a better type
+  title: string,
+}
+
+function SEO({ description = "", lang = "en", meta = [], title }: Props) {
   const { site } = useStaticQuery(
     graphql`
       query {
@@ -72,17 +78,6 @@ function SEO({ description, lang, meta, title }) {
   )
 }
 
-SEO.defaultProps = {
-  lang: `en`,
-  meta: [],
-  description: ``,
-}
 
-SEO.propTypes = {
-  description: PropTypes.string,
-  lang: PropTypes.string,
-  meta: PropTypes.arrayOf(PropTypes.object),
-  title: PropTypes.string.isRequired,
-}
 
 export default SEO
