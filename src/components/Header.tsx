584c55b7f81ddb6faf5e0a24ad87888f3a5c8c7f
Evan Brodie <brodie.evan@gmail.com>
2020-09-21 00:48:05 -0400

    Fixes linting issue

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,8 +4,6 @@ import { Typography } from "@material-ui/core"
 interface Props {
   siteTitle: string
 }
-const Header = ({ siteTitle }: Props) => (
-  <Typography variant="h2">{siteTitle}</Typography>
-)
+const Header = ({ siteTitle }: Props) => <Typography variant="h2">{siteTitle}</Typography>
 
 export default Header
