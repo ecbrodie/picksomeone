dd29d116ce8a95a58cf9ce2cd482edb6ce721460
Evan Brodie <brodie.evan@gmail.com>
2020-09-25 00:49:04 -0400

    Adds Github corner

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,12 +3,14 @@ import Layout from "../components/Layout"
 import SEO from "../components/SEO"
 import PickSomeone from "../components/PickSomeone"
 import { PersonListProvider } from "../contexts/PersonListContext"
+import GithubCorner from "react-github-corner"
 
 const IndexPage = () => (
   <PersonListProvider>
     <Layout>
       <SEO title="Picksome.one" />
       <PickSomeone />
+      <GithubCorner href="https://github.com/ecbrodie/picksomeone" />
     </Layout>
   </PersonListProvider>
 )
